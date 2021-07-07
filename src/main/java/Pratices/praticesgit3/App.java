1b314d0787cc5c18450ac3e14f676db378a417ff
Sonu-kumar98 <Sonukumar@github.com>
2021-07-07 18:27:37 +0530

    commit first one

---
+++ src/main/java/Pratices/praticesgit3/App.java
@@ -0,0 +1,15 @@
+package Pratices.praticesgit3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "yes sonu how are u  " );
+    }
+}
